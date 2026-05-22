88fef9821a5312991547f9afc0c7169aa6c53a05
dev-null-loop <dev.null.loop@gmail.com>
2026-05-22 10:32:21 +0300

    Refactor security recipe module

--- security_recipe/outputs.tf
+++ security_recipe/outputs.tf
@@ -1,4 +1,49 @@
 output "id" {
-  description = "Unique identifier that is immutable on creation"
+  description = "Unique identifier that can't be changed after creation"
   value       = oci_cloud_guard_security_recipe.this.id
+}
+
+output "compartment_id" {
+  description = "The OCID of the compartment that contains the recipe"
+  value       = oci_cloud_guard_security_recipe.this.compartment_id
+}
+
+output "description" {
+  description = "The recipe's description"
+  value       = oci_cloud_guard_security_recipe.this.description
+}
+
+output "display_name" {
+  description = "The recipe's display name"
+  value       = oci_cloud_guard_security_recipe.this.display_name
+}
+
+output "lifecycle_details" {
+  description = "A message describing the current state in more detail. For example, this can be used to provide actionable information for a recipe in the `Failed` state."
+  value       = oci_cloud_guard_security_recipe.this.lifecycle_details
+}
+
+output "owner" {
+  description = "The owner of the recipe"
+  value       = oci_cloud_guard_security_recipe.this.owner
+}
+
+output "security_policies" {
+  description = "The list of security policy IDs that are included in the recipe"
+  value       = oci_cloud_guard_security_recipe.this.security_policies
+}
+
+output "state" {
+  description = "The current lifecycle state of the recipe"
+  value       = oci_cloud_guard_security_recipe.this.state
+}
+
+output "time_created" {
+  description = "The time the recipe was created. An RFC3339 formatted datetime string."
+  value       = oci_cloud_guard_security_recipe.this.time_created
+}
+
+output "time_updated" {
+  description = "The time the recipe was last updated. An RFC3339 formatted datetime string."
+  value       = oci_cloud_guard_security_recipe.this.time_updated
 }
--- security_recipe/variables.tf
+++ security_recipe/variables.tf
@@ -1,17 +1,21 @@
 variable "compartment_id" {
-  description = "(Required) (Updatable) The compartment in which to create the recipe"
+  description = "(Required) (Updatable) The OCID of the compartment in which to create the recipe"
   type        = string
 }
 
 variable "display_name" {
-  description = "(Required) (Updatable) The recipe's name"
+  description = "(Required) (Updatable) The recipe's display name.\n\nAvoid entering confidential information."
   type        = string
 }
 
+variable "security_policies" {
+  description = "(Required) (Updatable) The list of security policy IDs to include in the recipe"
+  type        = list(string)
+}
+
 variable "defined_tags" {
-  description = "(Optional) (Updatable) Defined tags for this resource. Each key is predefined and scoped to a namespace."
+  description = "(Optional) (Updatable) Defined tags for this resource. Each key is predefined and scoped to a namespace. Example: `{\"foo-namespace.bar-key\": \"value\"}`"
   type        = map(string)
-  nullable    = true
   default     = null
 }
 
@@ -22,12 +26,7 @@ variable "description" {
 }
 
 variable "freeform_tags" {
-  description = "(Optional) (Updatable) Simple key-value pair that is applied without any predefined name, type or scope. Exists for cross-compatibility only."
+  description = "(Optional) (Updatable) Simple key-value pair that is applied without any predefined name, type or scope. Exists for cross-compatibility only. Example: `{\"bar-key\": \"value\"}`\n\nAvoid entering confidential information."
   type        = map(string)
   default     = {}
-}
-
-variable "security_policies" {
-  description = "(Required) (Updatable) The list of `SecurityPolicy` ids to include in the recipe"
-  type        = list(string)
 }
